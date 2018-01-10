b2cdd184713ea1ff91a3117db4678400016f6677
23832 <2383299053@qq.com>
2018-01-10 21:19:01 +0800

    二进制字面量

---
+++ src/语法/数值字面量的改进/BinaryIntegralLiteral.java
@@ -0,0 +1,11 @@
+package 语法.数值字面量的改进;
+
+/**
+ * 二进制整数字面量
+ * 0b或0B表示
+ */
+public class BinaryIntegralLiteral {
+    public static void main(String[] args) {
+        System.out.println(0b010101);
+    }
+}
