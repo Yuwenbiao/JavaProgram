2cef7bd10d7d9eab5e786a40620303254b6d5af4
ywb <2383299053@qq.com>
2018-01-13 21:35:09 +0800

    1

---
+++ src/语法/优化的异常处理/DataAccessGateway.java
@@ -0,0 +1,17 @@
+package 语法.优化的异常处理;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * 使用异常包装技术
+ */
+public class DataAccessGateway {
+//    public void load() throws DataAccessException {
+//        try {
+//            FileInputStream input = new FileInputStream("data.txt");
+//        } catch (IOException e) {
+//            throw new DataAccessException(e);
+//        }
+//    }
+}
---
+++ src/语法/数值字面量的改进/Underscore.java
@@ -0,0 +1,11 @@
+package 语法.数值字面量的改进;
+
+/**
+ * 在数值字面量中使用下划线
+ * 下划线必须在数字之间
+ */
+public class Underscore {
+    public static void main(String[] args) {
+        System.out.println(1_500_000);
+    }
+}
